92c9745aa3c5cc09400f05460df2db005e8f1628
Andrew Jessen-Tyler <andrewtyl@gmail.com>
2019-08-29 10:39:29 -0500

    fixed changes needed

--- src/bookmarkRouter.js
+++ src/bookmarkRouter.js
@@ -17,40 +17,48 @@ bookmarkRouter
   .get((req, res) => {
     const { id } = req.params;
     const returnBookmark = BOOKMARKS.filter(bookmark => bookmark.id === id);
-    if(!returnBookmark.length) {
+    if (!returnBookmark.length) {
       return res
         .status(404)
-        .json({error: {message: 'Item with the given ID does not exist'}});
-    }    
-    return res.json(returnBookmark);
+        .json({ error: { message: 'Item with the given ID does not exist' } });
+    }
+    else {
+      let toReturn = JSON.stringify(returnBookmark);
+      console.log(`toReturn is a ${typeof toReturn} with a value of ${toReturn}`)
+      toReturn = toReturn.slice(1, toReturn.length - 1);
+      console.log(`toReturn is a ${typeof toReturn} with a value of ${toReturn}`)
+      toReturn = JSON.parse(toReturn);
+      console.log(`toReturn is a ${typeof toReturn} with a value of ${toReturn}`)
+      return res.json(toReturn);
+    }
   });
 
 bookmarkRouter
   .route('/')
   .post(bodyParser, (req, res) => {
-    
-    const { title, description='', url, rating=1 } = req.body;
-    
-    if(!title) {
+
+    const { title, description = '', url, rating = 1 } = req.body;
+
+    if (!title) {
       return res
         .status(400)
-        .json({error: {message: 'Title is required'}});
+        .json({ error: { message: 'Title is required' } });
     }
 
-    if(!url) {
+    if (!url) {
       return res
         .status(400)
-        .json({error: {message: 'URL is required'}});
+        .json({ error: { message: 'URL is required' } });
     }
 
-    if(!url.includes('http://')) {
+    if ((!url.includes('http://')) && (!url.includes('https://'))) {
       return res
         .status(400)
-        .json({error: {message: 'URL must contain http://'}});
+        .json({ error: { message: 'URL must contain http:// or https://' } });
     }
 
     const newBookmark = {
-      id: cuid(), 
+      id: cuid(),
       title,
       description,
       url,
@@ -71,17 +79,17 @@ bookmarkRouter
   .delete((req, res) => {
     const { id } = req.params;
     const deleteIndex = BOOKMARKS.findIndex(bookmark => bookmark.id === id);
-    if(deleteIndex < 0) {
+    if (deleteIndex < 0) {
       return res
         .status(404)
-        .json({error: {message: 'Item with the given ID does not exist'}});
+        .json({ error: { message: 'Item with the given ID does not exist' } });
     }
 
     BOOKMARKS.splice(deleteIndex, 1);
 
     return res
       .status(202)
-      .json({message: 'Item successfully deleted'});
+      .json({ message: 'Item successfully deleted' });
   });
 
 module.exports = bookmarkRouter;
